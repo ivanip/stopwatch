b77d3bb6ad8abadb7a6f33b3b8453adf2769a44f
Ivan Ip <ivan.kw.ip@gmail.com>
2020-05-13 20:40:05 +0800

    圈速功能

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,108 @@
 <template>
     <div id="app">
-        <div id="stopwatch">00:00.0</div>
+        <div id="stopwatch">
+            {{ displayTime }}
+        </div>
         <div id="stopwatch-controls">
-            <button id="reset-and-lap">Reset</button>
-            <button id="start-and-stop">Start</button>
+            <button id="reset-and-lap" @click="resetAndLapHandler">{{ (isStarted && !isStopped) ? 'Lap' : 'Reset' }}</button> <!-- [3] -->
+            <button id="start-and-stop" @click="startAndStopHandler" :class="{ red: isStarted && !isStopped }">{{ (isStarted && !isStopped) ? 'Stop' : 'Start' }}</button>
             <span id="brand">Stopwatch</span>
         </div>
         <ul id="stopwatch-records">
-            <li>
-                <span>Lap 1</span>
-                <span>00:01.23</span>
+            <li :key="index" v-for="(lap, index) in laps"> <!-- [4] -->
+                <span>Lap {{ index + 1 }}</span>
+                <span>{{ lap.display }}</span>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-    // import HelloWorld from './components/HelloWorld.vue'
-
     export default {
-        name: 'App',
-        components: {
-            // HelloWorld
-        },
+        name: 'app',
+        components: {},
         data() {
             return {
                 timer: null,
                 displayTime: '00:00.0',
                 startTime: null,
+                stopTime: null,
+                stoppedTimeOffset: 0,
                 currentTime: null,
+                isStarted: false,
+                isStopped: false,
+                lastLapTime: null,  // [1]
+                laps: [],  // [2]
             }
         },
         methods: {
-            startAndStopHandler() {
-                this.startTime = Date.now()
-                this.timer = setInterval(() => {
-                    this.currentTime = Date.now();
-                    let elapsedTime = this.currentTime - this.startTime;
-                    this.displayTime=this.formatTime((elapsedTime/1000).toFixed(2))
+            startAndStopHandler() {  // [4]
 
-                })
-            }
-        }
+                if (!this.isStarted) {
+                    this.startTime = Date.now()
+                    this.lastLapTime=Date.now()
+
+                    this.timer = setInterval(() => {
+                        this.currentTime = Date.now();
+                        let elapsedTime = this.currentTime - this.startTime
+                        this.displayTime = this.formatTime((elapsedTime / 1000).toFixed(2))
+                    }, 10);
+
+                    this.isStarted = true
+                    this.isStopped = false
+                } else if (this.isStarted && !this.isStopped) {
+                    clearInterval(this.timer)
+                    this.stopTime = Date.now()
+                    this.isStopped = true
+                } else if (this.isStarted && this.isStopped) {
+                    this.stoppedTimeOffset += Date.now() - this.stopTime
+
+                    this.timer = setInterval(() => {
+                        this.currentTime = Date.now();
+                        let elapsedTime = this.currentTime - this.startTime - this.stoppedTimeOffset  // [6]
+                        this.displayTime = this.formatTime((elapsedTime / 1000).toFixed(2))
+                    }, 10);
+
+                    this.isStopped = false
+                }
+            },
+            resetAndLapHandler() {
+                if (this.isStarted && this.isStopped) {
+                    // Reset
+                    this.startTime = null
+                    this.stopTime = null
+                    this.stoppedTimeOffset = null
+                    this.displayTime = '00:00.00'
+                    clearInterval(this.timer)
+                    this.isStarted = false
+                    this.isStopped = false
+                } else if (this.isStarted && !this.isStopped) {
+                    // Lap
+                    let elapsedTime = this.currentTime - this.lastLapTime
+                    let lapTime = (elapsedTime / 1000).toFixed(2)
+
+                    this.laps.push({  // [4]
+                        time: parseFloat(lapTime),
+                        display: this.formatTime(lapTime),
+                    })
+
+                    this.lastLapTime = this.currentTime  // [5]
+                }
+            },
+            formatTime(seconds) {
+                let date = new Date(null);
+                date.setSeconds(seconds);
+                let result = date.toISOString().substr(14, 5);
+
+                return `${result}.${(seconds + '').split('.')[1]}`
+            },
+        },
     }
 </script>
 
 <style>
     #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         background-color: #000;
@@ -55,7 +110,7 @@
         height: 100%;
         display: flex;
         flex-direction: column;
-        color: #FFF;
+        color: #fff;
     }
 
     #stopwatch {
@@ -88,7 +143,7 @@
     }
 
     #stopwatch-records li.red {
-        color: #ff453a;
+        color: #FF453A;
     }
 
     #stopwatch-records li:first-child {
@@ -108,8 +163,7 @@
         flex: 80px 0 0;
         padding: .2rem 1.2rem;
         overflow: hidden;
-        position: relative;;
-        margin-bottom: 1.5rem;
+        position: relative;
         margin-bottom: 1.5rem;
     }
 
@@ -133,10 +187,16 @@
         float: left;
     }
 
-    button#start-and-stop.red {
-        background-color: #320e0b;
-        box-shadow: 0px 0px 0px 2px #320e0b;
-        color: #FF453A;
+    button#start-and-stop {
+        background-color: #082A12;
+        box-shadow: 0px 0px 0px 2px #082A12;
+        color: #2ED158;
+        float: right;
     }
 
+    button#start-and-stop.red {
+        background-color: #320E0B;
+        box-shadow: 0px 0px 0px 2px #320E0B;
+        color: #FF453A;
+    }
 </style>
